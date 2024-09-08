fa799bfb028ade8e2f12ad99d431905d87550c5f
Rj Toledo <rj.tldo@proton.me>
2024-09-08 23:41:03 +0700

    added nessus

--- install.sh
+++ install.sh
@@ -65,6 +65,41 @@ for tool in "${tools[@]}"; do
     fi
 done
 
+# Function to detect machine architecture and download Nessus accordingly
+download_nessus() {
+    ARCH=$(uname -m)
+    case $ARCH in
+        arm64)
+            echo "Detected ARM architecture."
+            curl --request GET \
+                --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.8.2-ubuntu1804_aarch64.deb' \
+                --output 'Nessus-10.8.2-ubuntu1804_aarch64.deb'
+            ;;
+        x86_64)
+            echo "Detected x64 architecture."
+            curl --request GET \
+                --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.8.2-ubuntu1604_amd64.deb' \
+                --output 'Nessus-10.8.2-ubuntu1604_amd64.deb'
+            ;;
+        *)
+            echo "Unsupported architecture: $ARCH"
+            exit 1
+            ;;
+    esac
+}
+
+# Call the function to download Nessus based on architecture
+download_nessus
+
+# Proceed with installation
+echo "Installing Nessus..."
+sudo dpkg -i Nessus-*.deb
+
+# Cleanup
+rm Nessus-*.deb
+
+echo "Nessus installed successfully!"
+
 # Final system update after tool installation
 echo "Performing final system update..."
 if apt update -y && apt upgrade -y; then
